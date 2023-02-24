0d858753aa7f47d90a4ae78a5966e8bc36c2bc33
scheslen <antgalin@mail.ru>
2023-02-24 12:05:17 +0300

    feat: create class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+    this.nMin=0;
+    this.nMax=0;
+    this.nTarget=0;
+    }
 
     setRange(min, max) {
-
+      this.nMin=min;
+      this.nMax=max;
     }
 
     guess() {
-
+      this.nTarget=Math.round((this.nMax+this.nMin)/2);
+      return this.nTarget;
     }
 
     lower() {
-
+       this.nMax=this.nTarget;
     }
 
     greater() {
-
+       this.nMin=this.nTarget;
     }
 }
 
